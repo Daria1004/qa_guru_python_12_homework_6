5ae2f87e8ee5ddbf49b7e61e5e25f095c08cf0f6
Daria Jakuszewicz <d.jakuszewicz@gmail.com>
2024-03-28 10:35:26 +0300

    if 6 < current_time.hour < 22: -- нужно чуть другое условие, всё-таки 6 уже "шесть утра".
    Исправлено.

--- tests/homework_6.py
+++ tests/homework_6.py
@@ -10,7 +10,7 @@ def test_dark_theme_by_time():
 
     is_dark_theme = None
 
-    if 6 < current_time.hour < 22:
+    if 6 <= current_time.hour < 22:
         is_dark_theme = False
     else:
         is_dark_theme = True
